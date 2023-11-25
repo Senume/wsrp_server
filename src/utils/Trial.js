5c215aefcb4481832b8bd585b89f83bafc72a932
Visweswaran <viswesm2@gmail.com>
2023-11-25 12:33:37 +0530

    Added Streaming Songs

--- src/utils/Trial.js
+++ src/utils/Trial.js
@@ -2,41 +2,48 @@ import Database from "./DatabaseClass.js";
 
 const DataBase = new Database("mongodb://0.0.0.0:27017", "Songs");
 
-await DataBase.UsersDB.AddUser({
-  name: "Viswes",
-  age: 16,
-});
-
+// User
 console.log(await DataBase.UsersDB.Finduser("Viswes"));
 
-await DataBase.UsersDB.UpdateUser("Viswes", { age: 18, gender: "male" });
-
-await DataBase.PlaylistDB.MakePlaylist({
-  name: "Liked Songs",
-  songs: ["The Nights", "The Oldies", "The newons"],
+await DataBase.UsersDB.UpdateUser({
+  username: "Viswes",
+  age: 18,
+  gender: "male",
 });
 
-await DataBase.PlaylistDB.UpdatePlaylist(
-  "Liked Songs",
-  "Viswes",
-  ["The Nights", "The Oldies"],
-  "remove"
-);
+// Playlist
+await DataBase.PlaylistDB.UpdatePlaylist({
+  name: "Liked Songs",
+  editor: "Chubramani",
+  decision: "add",
+  songs: ["The Nights", "The mornings"],
+});
 
 console.log(DataBase.PlaylistDB.FindPlaylist("The Nights"));
 
+// History
 await DataBase.HistoryDB.UpdateHistory("Viswes", ["The T", "The U", "The V"]);
 
-await DataBase.SongsDB.uploadMP3File(
+// Songs
+import Songs from "./Collections/Songs.js"; // Assuming Songs class is in a separate file
+const songs = new Songs("mongodb://0.0.0.0:27017", "Songs");
+
+// upload file
+const fileId = await songs.uploadMP3File(
   "C:/Users/Visweswaran/Downloads/Nights.mp3",
   {
-    filename: "Nights.mp3",
-    title: "The Nights",
-    artist: "Avici",
+    title: "TheNights",
+    artist: "Viswes",
   }
 );
+console.log("Uploaded file ID:", fileId);
+const song = new Songs("mongodb://0.0.0.0:27017", "Songs");
 
+// streaming endpoint
+song.createStreamingEndpoint();
+
+// download
 await DataBase.SongsDB.downloadMP3File(
-  "The Nights",
+  "TheNights",
   "C:/Users/Visweswaran/Downloads/FromNights.mp3"
 );
